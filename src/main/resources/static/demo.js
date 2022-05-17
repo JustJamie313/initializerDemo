c338d9be15d5bc8119e87918b30d6441122cca4b
KCCoderGirl <dontlaughatme@yahoo.com>
2022-05-17 12:12:16 -0500

    working version

--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -1,11 +1,9 @@
 function runDemo(){
-    let outputData = document.querySelector("#jsReturnData");
-    outputData.innerHTML = outputData.getAttribute("data-demo").replace("Java Controller","Javascript Function");
-    let outputList = document.querySelector("#jsReturnList");
-    let list = outputList.getAttribute("data-list");
-    list.replace("[","").replace("]","");
-    list = list.split(",");
-    for(let a=0;a<list.length;a++){
-        outputList.innerHTML += `<li> The Javascript function changed ${list[a]} to ${list[a].replace("Java","JavaScript")}</li>`;
+    let outputDataElement = document.querySelector("#jsReturnData");
+    outputDataElement.innerHTML = outputDataElement.getAttribute("data-demo").replace("Java Controller","Javascript Function");
+    let outputDataListElement = document.querySelector("#jsReturnList");
+    let list = outputDataListElement.getAttribute("data-list").split(",");
+    for(let item in list){
+        outputDataListElement.innerHTML += `<li>${list[item].replace("Java","Javascript").replace("[","").replace("]","")}</li>`;
     }
 }
