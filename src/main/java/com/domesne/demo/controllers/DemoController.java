f4c57a4354d9166d0d0b9c6fd8f959502b5d6727
KCCoderGirl <dontlaughatme@yahoo.com>
2022-05-17 12:26:03 -0500

    working version - with notes

--- src/main/java/com/domesne/demo/controllers/DemoController.java
+++ src/main/java/com/domesne/demo/controllers/DemoController.java
@@ -9,14 +9,21 @@ import java.util.List;
 
 @Controller
 public class DemoController {
+//    Map to localhost:8080
     @GetMapping
     public String runDemo(Model model){
+
+//        Create demoList and add sample data
         List<String> demoList = new ArrayList<>();
         demoList.add("Java List 1");
         demoList.add("Java List 2");
         demoList.add("Java List 3");
+
+//        Add sample demoData and demoList to model
         model.addAttribute("demoData","This string and list are passed from Java Controller");
         model.addAttribute("demoList",demoList);
+
+//        Return template
         return "demo";
     }
 }
--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -1,8 +1,17 @@
 function runDemo(){
+    //get element to display output of demoData(variable from Java)
     let outputDataElement = document.querySelector("#jsReturnData");
+
+    //set innerHTML of element to demoData (stored in data-demo attribute) replacing a substring
     outputDataElement.innerHTML = outputDataElement.getAttribute("data-demo").replace("Java Controller","Javascript Function");
+
+    //get element to display output of demoList(variable from Java)
     let outputDataListElement = document.querySelector("#jsReturnList");
+
+    //get demoList (stored in data-list attribute) and split into array
     let list = outputDataListElement.getAttribute("data-list").split(",");
+
+    //loop through list array and add <li> element to demoList output element replacing substring and []'s
     for(let item in list){
         outputDataListElement.innerHTML += `<li>${list[item].replace("Java","Javascript").replace("[","").replace("]","")}</li>`;
     }
